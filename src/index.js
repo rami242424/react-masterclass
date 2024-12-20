05117fbaac974b61bcb9c38d89c3600c84ccdf06
Rami <138556019+rami242424@users.noreply.github.com>
2024-12-20 20:33:39 +0900

    #2.1 Our First Styled Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // 'react-dom/client'에서 createRoot 가져오기
 import App from './App';
 
-ReactDOM.render(
+// 'root' 엘리먼트를 가져옴
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// createRoot를 사용하여 렌더링
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
