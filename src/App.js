4cc07a01eddaa3e48e91d46903a47dee0f46f3b3
Rami <138556019+rami242424@users.noreply.github.com>
2024-12-23 17:36:36 +0900

    darkMode , light mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import { useState } from "react";
+import styled, {ThemeProvider} from "styled-components";
 
 const Title = styled.h1`
   color: ${ (props) => props.theme.textColor};
@@ -13,11 +14,33 @@ const Wrapper = styled.div`
   background-color: ${ (props) => props.theme.backgroundColor}
 `;
 
+const ToggleBtn = styled.button`
+  color: ${ (props) => props.theme.textColor};
+  background-color: ${ (props) => props.theme.backgroundColor}
+`;
+
+const darkTheme = {
+  textColor : "whitesmoke",
+  backgroundColor : "#111",
+}
+const lightTheme = {
+  textColor : "#111",
+  backgroundColor : "whitesmoke",
+}
+
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  const toggleTheme = () => setIsDarkMode((prev) => !prev);
+
   return (
-    <Wrapper>
-      <Title>Helloooooo</Title>
-    </Wrapper>
+    <ThemeProvider theme = {isDarkMode ? darkTheme : lightTheme}>
+      <Wrapper>
+        <Title>Helloooooo</Title>
+        <ToggleBtn onClick={toggleTheme}>
+          {isDarkMode ? "change to light theme" : "change to dark theme"}
+        </ToggleBtn>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
