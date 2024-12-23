2fe6adff46b8a8829f897fd23a788433c5e709af
Rami <138556019+rami242424@users.noreply.github.com>
2024-12-23 17:18:03 +0900

    #2.7 Themes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // 'react-dom/client'에서 createRoot 가져오기
+// 'react-dom/client'에서 createRoot 가져오기
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 
 // 'root' 엘리먼트를 가져옴
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const darkTheme = {
+  textColor : "whitesmoke",
+  backgroundColor: "#111",
+}
+
+const lightTheme = {
+  textColor : "#111",
+  backgroundColor: "whitesmoke",
+}
+
+
 // createRoot를 사용하여 렌더링
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
