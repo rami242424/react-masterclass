a9405c7a5bdf5143ad45dbb4d7e57673cf7b65d1
Rami <138556019+rami242424@users.noreply.github.com>
2024-12-24 21:12:06 +0900

    # 3.2 Typing the Props

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
-// 'react-dom/client'에서 createRoot 가져오기
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
 import App from './App';
 
-// 'root' 엘리먼트를 가져옴
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-const darkTheme = {
-  textColor : "whitesmoke",
-  backgroundColor: "#111",
-}
 
-const lightTheme = {
-  textColor : "#111",
-  backgroundColor: "whitesmoke",
-}
-
-
-// createRoot를 사용하여 렌더링
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
