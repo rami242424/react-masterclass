55f1b94451c6912d14fc770c0dd3563e7ccabbe7
Rami <138556019+rami242424@users.noreply.github.com>
2024-12-25 18:54:15 +0900

    react-router-dom 설치

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from './theme';
+import { Theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={Theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
